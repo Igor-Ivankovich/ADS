4a24b645ec3ba89c3c731285b12b5f844c0161e3
Ivankovich.I <ihar.ivankovich@gmail.com>
2022-01-08 19:30:02 +0300

    added support for saving pictures

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,6 +62,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :post_type, :user_id)
+      params.require(:post).permit(:title, :description, :post_type, :user_id, :content)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  # has_rich_text :content
+  has_rich_text :content
   before_save :assign_post_type
 
   def assign_post_type
